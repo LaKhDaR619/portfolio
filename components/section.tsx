44c8db39b4a95b9dcbfc6640bb54cf38afa8e783
Lakhdar Ben Boudina <lakhdar.boudina@gmail.com>
2021-08-14 03:26:46 +0100

    fixed a bug with scrolling stop

--- components/section.tsx
+++ components/section.tsx
@@ -1,5 +1,5 @@
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   id: string;
@@ -21,11 +21,21 @@ const Section: React.FC<Props> = ({
     const sectionHeight = ref.current.offsetHeight;
     const sectionTop = ref.current.offsetTop - 50;
 
+    // if (
+    //   currPos.y * -1 + 10 >= prevPos.y * -1 &&
+    //   prevPos.y * -1 + 10 >= currPos.y * -1
+    // )
+    //   console.log("stop detected");
+
     if (
       currPos.y * -1 > sectionTop &&
-      currPos.y * -1 <= sectionTop + sectionHeight
-    )
+      currPos.y * -1 <= sectionTop + sectionHeight &&
+      // detect scroll stop
+      currPos.y * -1 + 10 >= prevPos.y * -1 &&
+      prevPos.y * -1 + 10 >= currPos.y * -1
+    ) {
       if (setActiveNavItem) setActiveNavItem(id);
+    }
   });
 
   return (
